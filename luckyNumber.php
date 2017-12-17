430422eb77bb8e598ea25bce57218a91352356a6
hzhyvinskyi <32293052+hzhyvinskyi@users.noreply.github.com>
2017-12-17 23:47:25 +0200

    Update luckyNumber.php

--- luckyNumber.php
+++ luckyNumber.php
@@ -2,9 +2,9 @@
 
 function lucky($num)
 {
-  if ($num == rand(1, 4)) {
+  if ($num == rand(1, 3)) {
     return "You are lucky!";
-  } elseif ($num < 1 || $num > 4) {
+  } elseif ($num < 1 || $num > 3) {
     return "Unexpected number";
   } else {
     return "Sorry, you are not lucky. Try again!";
